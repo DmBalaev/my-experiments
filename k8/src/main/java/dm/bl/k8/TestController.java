b44c7153dfa13d667d1a61e043018d538714e8ad
Akira <Akira@>
2025-04-21 17:27:24 +0300

    K8-1: init template

---
+++ k8/src/main/java/dm/bl/k8/TestController.java
@@ -0,0 +1,15 @@
+package dm.bl.k8;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+    @GetMapping
+    public String test() {
+        return "Hello World from K8";
+    }
+}
